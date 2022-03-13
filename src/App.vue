26d548c90af0d06bcb1c7c6fb5dbf18711288f14
Lana Schuster <lanaschuster1@gmail.com>
2022-03-12 21:33:48 -0300

    feat: theme setup; added layout component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 
 <template>
-  <h1>hello world!</h1>
+  <main>
+    <TimeDashboardLayout>
+      <template #side-column>
+        <div>
+          side column
+        </div>
+      </template>
+      <template #main-grid>
+        <div>
+          main grid
+        </div>
+      </template>
+    </TimeDashboardLayout>
+  </main>
 </template>
 
 <script>
+import TimeDashboardLayout from '@/layouts/time-dashboard-layout/time-dashboard-layout.vue'
 export default {
-  
+  components: {
+    TimeDashboardLayout
+  }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  font-family: $font-family;
+}
+body, #app {
+  height: 100vh;
+  background-color: $theme-darkest-color;
+}
+main {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/layouts/time-dashboard-layout/time-dashboard-layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="side-column">
+      <slot name="side-column"></slot>
+    </div>
+    <div class="main-grid">
+      <slot name="main-grid"></slot>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: $grid-gap;
+}
+</style>
